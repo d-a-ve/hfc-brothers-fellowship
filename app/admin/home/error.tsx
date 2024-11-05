6cdd55a240b6f2c9861e8cb246a2af1fa7b6c78a
Dave <davearonmwan@gmail.com>
2024-11-05 22:57:59 +0100

    fix deployment problem

--- app/admin/home/error.tsx
+++ app/admin/home/error.tsx
@@ -9,6 +9,7 @@ export default function DashboardErrorBoundary({
 	error: Error & { digest?: string }
 	// reset: () => void
 }) {
+	console.error(error)
 	return (
 		<div className="mx-auto flex min-h-[calc(100dvh_-_200px)] max-w-[40ch] flex-col items-center justify-center gap-4 text-center">
 			<h1 className="pb-3 font-semibold ~text-xl/4xl">
